26f422503559eb6de774c06e581e9b1a7ebaff95
javierfm <jadosfer@hotmail.com>
2021-05-03 11:50:43 -0300

    slider horizontal va al final y vuelve

--- backgroundRotation.js
+++ backgroundRotation.js
@@ -1,6 +1,6 @@
 var imageID=0;
 
-function changeimage(every_seconds){
+function changeimage2(every_seconds){
     //change the image
     if(!imageID){
         document.getElementById("slideIn").src = "assets/images/buda1.jpg";  
@@ -33,48 +33,6 @@ function changeimage(every_seconds){
 }
 
 
-function changeimage2(every_seconds){
-    
-    // document.getElementById("buda").style.display = "block";
-    // document.getElementById("chamanes").style.display = "block";
-    // document.getElementById("pescador").style.display = "block";
-    // document.getElementById("fuego").style.display = "block";
-    // document.getElementById("montania").style.display = "block";
-
-    if(!imageID){        
-        document.getElementById("buda").classList.add('slideOut');     
-        document.getElementById("buda").style.display = "none";          
-   
-        imageID++;
-    }else{if(imageID==1){    
-        //document.getElementById("buda").classList.remove('slideOut');
-           
-        document.getElementById("chamanes").classList.add('slideOut');        
-        
-         
-        imageID++;                    
-    }else{if(imageID==2){
-       // document.getElementById("chamanes").classList.remove('slideOut');
-        //document.getElementById("chamanes").style.display = "none";
-        document.getElementById("pescador").classList.add('slideOut'); 
-        imageID++;
-      
-    }else{if(imageID==3){     
-        document.getElementById("pescador").style.display = "none";
-        document.getElementById("fuego").classList.add('slideOut'); 
-        
-        imageID++;
-        
-    }else{if(imageID==4){      
-        document.getElementById("fuego").style.display = "none";
-        document.getElementById("montania").classList.add('slideOut'); 
-        
-        imageID=0;
-       
-    }}}}}
-    //call same function again for x of seconds
-    setTimeout("changeimage("+every_seconds+")",((every_seconds)*1000));
-}
 
 
 
